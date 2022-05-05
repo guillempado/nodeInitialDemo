a17f731007bb0769a52e58e8b4a82f4a28f7ee42
LauraOrte <laura.orte7@gmail.com>
2022-05-05 09:19:30 +0200

    /user arreglado

--- controllers/user.js
+++ controllers/user.js
@@ -1,8 +1,9 @@
 const userGet = (req, res) => { 
+    let fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
     res.json({
         nom : 'Laura',
         edad: 31,
-        url:'http://localhost:9000/'
+        fullUrl,
     }); 
   };
 
