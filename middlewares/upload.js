336daa2442d27be80200079d4ba6076dd40c739e
LauraOrte <laura.orte7@gmail.com>
2022-05-05 12:29:34 +0200

    Listo!!!

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -1,12 +1,19 @@
+const res = require('express/lib/response');
 const multer = require('multer')
 const path = require('path')
 
-let fileFilter = (req, file, cb) => {
+let fileFilter = (req, res = response, cb) => {
+    if (req.files){
     const fileTypes = ['image/png', 'image/jpg', 'image/jpeg', 'image/gif'];
 
     if (fileTypes.some(fileType => fileType === req.files.image.mimetype)) return cb(null, true);
 
-    return cb(new Error('Valid files only with the following extensions .jpeg/.jpg, .png and .gif'), false);
+    return res.status(400).json({ message: "Valid files only with the following extensions .jpeg/.jpg, .png and .gif"});
+ 
+    } else {
+        res.status(400).json({ message: "no hay ningún archivo"});
+
+ }
 }
 
 module.exports = fileFilter;
